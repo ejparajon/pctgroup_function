9171d09a2b6438883b504a2dad80735387d91187
Eric Parajon <ericparajon@gmail.com>
2025-01-01 13:47:00 -0500

    updating examples

--- pctgroup_function.R
+++ pctgroup_function.R
@@ -5,25 +5,34 @@ if(!require(tidyverse)){
   library(tidyverse)
 }
 
-
 # Creating the function ---------------------------------------------------
 
-#This function enables quick data summarizing and grouping
-pctgroup <- function(data,...,rd = 1) {
+#This function enables quick summarizing and grouping of catagorical variables
+pctgroup <- function(data,...,rd = 1) { #rd sets rounding in this case to 1 digit after decimal
   dplyr::select(data,...) %>% #select the data and variables of interest
-    dplyr::filter(complete.cases(.)) %>% #drop any missing data in this case the . is referring to the dataframe
-    dplyr::group_by(...,.drop=FALSE) %>% #optional select a grouping variable (ignoring dropping groups that don't appear)
+    dplyr::filter(complete.cases(.)) %>% #drop any missing data in this case the . is referring to the data
+    dplyr::group_by(...,.drop=FALSE) %>% #Adds the option to select a grouping variable (ignoring dropping groups that don't appear)
     dplyr::summarise(n = n()) %>% #Summarise each group down to one row to generate the number of obs 
     mutate(Percentage = round(100*n/sum(n),rd)) %>% #Creating a percentage variable based on the number of obs
     ungroup %>% 
-    complete(...,fill=list(n=0,Percentage=0)) #Combined with line 15 this returns observations that do not show as zeros rather than missing
+    complete(...,fill=list(n=0,Percentage=0)) #Combined with line 15 this returns observations that are options in the underlying data but that do not show up as zeros in the final output
 }
 
 
 # Example usage ---------------------------------------------------
-print(pctgroup(starwars,homeworld),n=Inf) #generating a list of character's homeworlds present in the starwars films (counts and then percentage of all homeworlds)
+#Generic usage: pctgroup(data,variable)
+#The first variable listed is the grouping variable. 
 
-print(pctgroup(starwars,species,gender),n=Inf) #Are some starwars species more likely to be male or female? This also demonstrates the value of including missing observations (some species feature only one gender)
+#1.generating a list of character's homeworlds present in the starwars films (counts and then percentage of all homeworlds)
+print(pctgroup(starwars,homeworld),n=Inf) 
 
+#1.1 can also add a call to arrange() to order the output
+print(pctgroup(starwars,homeworld),n=Inf) %>% 
+  arrange(desc(Percentage))
 
-print(pctgroup(diamonds,color,cut),n=Inf) #Breakdown of types of diamond cuts according to color.
+#2. Are some starwars species more likely to be male or female? 
+#This also demonstrates the value of including missing observations (some species feature only one gender)
+print(pctgroup(starwars,species,gender),n=Inf) 
+
+#3.Breakdown of types of diamond cuts according to color.
+print(pctgroup(diamonds,color,cut),n=Inf)
